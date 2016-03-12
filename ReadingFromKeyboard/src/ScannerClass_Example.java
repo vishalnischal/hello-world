2ae9f87396781931bcf80cbaa751ef054122e9ae
vishalnischal <vishalnischal@192.168.1.2>
2016-03-13 00:55:29 +0530

    Added project in Git

---
+++ ReadingFromKeyboard/src/ScannerClass_Example.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+
+public class ScannerClass_Example {
+
+	public static void main(String[] args)
+	{
+	//	System.out.println(Math.pow(2, 5));
+		
+		Scanner sr = new Scanner(System.in);
+		System.out.print("Enter the String : ");
+		String str = sr.nextLine();
+	//	System.out.println("You Entered the String : "+str);
+		
+		System.out.print("Enter an Integer: ");
+		int s = sr.nextInt();
+	//	System.out.println("You Entered the integer : "+s);
+
+		
+		
+		
+	}
+
+}
